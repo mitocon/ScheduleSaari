60d03f912ab27a6f8f05e41c538838fe6ea98e46
mitocon <mitocon496@gmail.com>
2023-06-11 20:31:08 +0900

    スケジュール登録機能、日付ごとの分別 TextFieldの最初の"/"での処理

--- ScheduleSaari/ContentView.swift
+++ ScheduleSaari/ContentView.swift
@@ -1,26 +1,60 @@
-//
-//  ContentView.swift
-//  ScheduleSaari
-//
-//  Created by Itoi_Dev on 2023/06/11.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textFieldInput: String = ""
+    @State private var listItems: [String: [String]] = [:]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            List {
+                ForEach(listItems.keys.sorted(), id: \.self) { key in
+                    Section(header: Text("List \(key)")) {
+                        ForEach(listItems[key] ?? [], id: \.self) { item in
+                            Text(item)
+                        }
+                    }
+                }
+            }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Spacer()
+
+            HStack {
+                Spacer()
+
+                TextField("テキストを入力してください", text: $textFieldInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                Button(action: {
+                    let rawDayString = String(textFieldInput.prefix(5))
+                    var dayString = ""
+                    
+                    
+                    // 前3文字以内に"/"の有無の確認が必要
+                    // 全体にスペースの有無の確認が必要
+                    
+                    
+                    // dayStringに"5/2", "6/14"などの日付を代入する。
+                    if let slashIndex = rawDayString.firstIndex(of: "/") {
+                        let startIndex = rawDayString.index(slashIndex, offsetBy: -2, limitedBy: rawDayString.startIndex) ?? rawDayString.startIndex
+                        let endIndex = rawDayString.index(slashIndex, offsetBy: 3, limitedBy: rawDayString.endIndex) ?? rawDayString.endIndex
+                        dayString = String(rawDayString[startIndex..<slashIndex]) + String(rawDayString[slashIndex..<endIndex])
+                    }
+
+                    listItems[dayString, default: []].append(textFieldInput)
+                    textFieldInput = ""
+                }) {
+                    Text("登録")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+
+                Spacer()
+            }
+
+            Spacer()
+        }
     }
 }
--- ScheduleSaari/ScheduleSaariApp.swift
+++ ScheduleSaari/ScheduleSaariApp.swift
@@ -1,14 +1,7 @@
-//
-//  ScheduleSaariApp.swift
-//  ScheduleSaari
-//
-//  Created by Itoi_Dev on 2023/06/11.
-//
-
 import SwiftUI
 
 @main
-struct ScheduleSaariApp: App {
+struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
