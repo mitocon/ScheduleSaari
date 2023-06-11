fdc0a30516674cbe7e67c2e33ff981c78d9a95ee
mitocon <mitocon496@gmail.com>
2023-06-11 12:43:54 +0900

    初回コミット

---
+++ ScheduleSaari/ScheduleSaariApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleSaariApp.swift
+//  ScheduleSaari
+//
+//  Created by Itoi_Dev on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleSaariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
