94e02322619604c212034c554727b0e5e65f2d7e
mitocon <mitocon496@gmail.com>
2023-06-13 23:30:06 +0900

    ListのキーをDate型に修正 付随して6/01と6/1が別Listになる問題を修正。 未来の日付だけを予定登録できるよう修正。

--- ScheduleSaari/ContentView.swift
+++ ScheduleSaari/ContentView.swift
@@ -1,62 +1,38 @@
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     @State private var textFieldInput: String = ""
-    @State private var listItems: [String: [String]] = [:]
-
+    @State private var listItems: [Date: [String]] = [:]
+    
     var body: some View {
         VStack {
             List {
-                ForEach(listItems.keys.sorted(), id: \.self) { key in
-                    Section(header: Text("List \(key)")) {
-                        ForEach(listItems[key] ?? [], id: \.self) { item in
-                            Text(item)
-                                .contextMenu {
-                                    Button(action: {
-                                        // Edit action
-                                        print("Edit item: \(item)")
-                                    }) {
-                                        Text("編集")
-                                        Image(systemName: "pencil")
-                                    }
-
-                                    Button(action: {
-                                        // Delete action
-                                        listItems[key]?.removeAll(where: { $0 == item })
-                                        if listItems[key]?.isEmpty ?? false {
-                                            listItems[key] = nil
-                                        }
-                                    }) {
-                                        Text("削除")
-                                        Image(systemName: "trash")
-                                    }
-                                }
+                ForEach(listItems.sorted(by: { $0.key < $1.key }), id: \.key) { key, items in
+                    Section(header: Text("List \((key))")) { // 日付を表示するためにフォーマット
+                        ForEach(items, id: \.self) { item in
+                            ListItemView(item: item) {
+                                handleEditItem(item)
+                            } onDelete: {
+                                handleDeleteItem(item, for: key)
+                            }
                         }
                     }
                 }
             }
-
+            
+            
             Spacer()
-
+            
             HStack {
                 Spacer()
-
+                
                 TextField("テキストを入力してください", text: $textFieldInput)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-
+                
                 Button(action: {
-                    let rawDayString = String(textFieldInput.prefix(5))
-                    var dayString = ""
-
-                    if let slashIndex = rawDayString.firstIndex(of: "/") {
-                        let startIndex = rawDayString.index(slashIndex, offsetBy: -2, limitedBy: rawDayString.startIndex) ?? rawDayString.startIndex
-                        let endIndex = rawDayString.index(slashIndex, offsetBy: 3, limitedBy: rawDayString.endIndex) ?? rawDayString.endIndex
-                        dayString = String(rawDayString[startIndex..<slashIndex]) + String(rawDayString[slashIndex..<endIndex])
-                    }
-
-                    listItems[dayString, default: []].append(textFieldInput)
-                    textFieldInput = ""
+                    registerItem()
                 }) {
                     Text("登録")
                         .padding()
@@ -64,12 +40,110 @@ struct ContentView: View {
                         .background(Color.blue)
                         .cornerRadius(10)
                 }
-
+                
                 Spacer()
             }
-
+            
             Spacer()
         }
+    }
+    
+    private func handleEditItem(_ item: String) {
+        print("Edit item: \(item)")
+    }
+    
+    private func handleDeleteItem(_ item: String, for key: Date) {
+        listItems[key]?.removeAll(where: { $0 == item })
+        if listItems[key]?.isEmpty ?? false {
+            listItems[key] = nil
+        }
+    }
+    
+    private func registerItem() {
+        var dayString = ""
+        
+        if let slashIndex = textFieldInput.firstIndex(of: "/") {
+            let startIndex = textFieldInput.index(slashIndex, offsetBy: -2, limitedBy: textFieldInput.startIndex) ?? textFieldInput.startIndex
+            let endIndex = textFieldInput.index(slashIndex, offsetBy: 3, limitedBy: textFieldInput.endIndex) ?? textFieldInput.endIndex
+            let mString = String(textFieldInput[startIndex..<slashIndex])
+            var dString = String(textFieldInput[slashIndex..<endIndex])
+            dString.removeFirst()
+            
+            if mString.first == "0" {
+                dayString += String(mString.dropFirst())
+            } else {
+                dayString += mString
+            }
+            
+            dayString += "/"
+            
+            if dString.first == "0" {
+                dayString += String(dString.dropFirst())
+            } else {
+                dayString += dString
+            }
+        }
+        print(dayString)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M/d"
+        
+        let currentDate = Date()
+        
+        let currentCalendar = Calendar.current
+        let currentComponents = currentCalendar.dateComponents([.year, .month, .day], from: currentDate)
+        
+        if let dayDate = dateFormatter.date(from: dayString) {
+            let targetComponents = currentCalendar.dateComponents([.month, .day], from: dayDate)
+            var futureComponents = DateComponents()
+            
+            if let currentMonth = currentComponents.month, let currentDay = currentComponents.day,
+               let targetMonth = targetComponents.month, let targetDay = targetComponents.day {
+                
+                if currentMonth > targetMonth || (currentMonth == targetMonth && currentDay > targetDay) {
+                    // 未来の年になるように設定
+                    futureComponents.year = currentComponents.year! + 1
+                } else {
+                    futureComponents.year = currentComponents.year
+                }
+                
+                futureComponents.month = targetMonth
+                futureComponents.day = targetDay
+                
+                if let futureDate = currentCalendar.date(from: futureComponents) {
+                    // dayDateをDate型として使用できる
+                    // ここでlistItemsへのアクセスや操作を行う
+                    listItems[futureDate, default: []].append(textFieldInput)
+                    textFieldInput = ""
+                } else {
+                    print("日付の変換に失敗しました。")
+                }
+            } else {
+                print("日付の変換に失敗しました。")
+            }
+        } else {
+            print("日付の変換に失敗しました。")
+        }
+    }
+}
+
+struct ListItemView: View {
+    let item: String
+    let onEdit: () -> Void
+    let onDelete: () -> Void
+    
+    var body: some View {
+        Text(item)
+            .contextMenu {
+                Button(action: onEdit) {
+                    Text("編集")
+                    Image(systemName: "pencil")
+                }
+                
+                Button(action: onDelete) {
+                    Text("削除")
+                    Image(systemName: "trash")
+                }
+            }
     }
 }
 
